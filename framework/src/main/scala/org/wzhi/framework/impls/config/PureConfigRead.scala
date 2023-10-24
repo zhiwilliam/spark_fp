63676b1437346966e18875e992c283d90681a374
zhiwi <zhiwilliam@gmail.com>
2023-10-24 09:24:20 -0400

    first commit

--- framework/src/main/scala/org/wzhi/framework/impls/config/PureConfigRead.scala
+++ framework/src/main/scala/org/wzhi/framework/impls/config/PureConfigRead.scala
@@ -18,6 +18,9 @@ object PureConfigRead {
     def fromFile(filePath: String): IO[A]
   }
 
+  // todo: This method only reads from classpath, we need a way to read from absolute file path.
+  // Also, think about read from http or S3 bucket.
+
   def read[A: ClassTag : ConfigReader]: ReadFileWithIO[A] =
     (filePath: String) => FileRead.makeResourceForRead(filePath).use(x =>
       PureConfigRead.readConfFromString[IO, A](x.getLines().mkString("\n")))
